a0bc57470d575b211ba22e6f77a9ca9963c54637
JasCoder <85804831+Fanu2@users.noreply.github.com>
2025-08-06 16:59:27 -0500

    Update build_index.py

--- build_index.py
+++ build_index.py
@@ -2,7 +2,7 @@
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.embeddings import OpenAIEmbeddings
 
 # --- Load PDF ---
